7b912bfe7e404f228201e0835ac4896b6bfc5dd7
sreeramraghu04 <sreeramraghu04@gmail.com>
2026-02-21 13:45:01 +0530

    final

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ export const instance = new Razorpay({
   key_id: config.RAZOR_KEY_ID,
   key_secret: config.RAZOR_KEY_SECRET,
 });
+
 const PORT = config.PORT;
+
 app.listen(PORT, () => {
   console.log(`App is running at ${PORT}`);
 });
